a7c07ec985da66502f5f088bf5f1d5987adeea1b
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-05-11 20:09:02 +0530

    Project Started With Basic UI

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  const handleEditClick = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            onChange={(e) => setPlayerName(e.target.value)}
+            value={playerName}
+            required
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
