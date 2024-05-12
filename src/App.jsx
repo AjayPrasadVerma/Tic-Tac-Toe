1da47d4fc4dea3e0e20abf2c97c2fb2ac725311d
AjayPrasadVerma <imajvermaofficial@gmail.com>
2024-05-12 15:20:28 +0530

    game board component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Player from "./components/Player";
 
+import GameBoard from "./components/GameBoard";
+
 function App() {
   return (
     <main>
@@ -8,7 +10,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   );
